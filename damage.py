24ea1e34a76d3d4d66bfcb9df947c23fe965172e
Yanottama Oktabrian <oktatama4@gmail.com>
2020-07-09 23:10:31 +0700

    Update damage list

---
+++ damage.py
@@ -0,0 +1,17 @@
+# damage taken from attack
+
+# mage
+# attack magic - 30, stamina - 5
+
+
+# warrior
+# attack magic - 0, stamina - 30
+
+
+
+# priest
+# attack magic - 20, stamina - 10
+
+# hunter
+# attack magic - 20, stamina - 20
+
