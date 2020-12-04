4ab67ab4231591d2b97afe1867e7cb00c08b492f
yanottamao <oktatama4@gmail.com>
2020-12-05 06:14:47 +0700

    Edit function call

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import player
+import player_attribute
 
-pl_name = player.player_name()
-pl_stat = player.player_job()
-print('Player name is ' + pl_name)
-print('Player job is ' + pl_stat)
+
+def main():
+    # correct way to call return
+    pl_name = player.player_name()
+    pl_stat = player.player_job()
+    pl_attr = ''
+
+    print('\nNATURPG GAMES\n')
+    print('Player name is ' + pl_name)
+    print('Player job is ' + pl_stat + '\n')
+    if pl_stat == 'fighter':
+        pl_attr = player_attribute.player_fighter()
+    elif pl_stat == 'swordsman':
+        pl_attr = player_attribute.player_swordsman()
+    elif pl_stat == 'mage':
+        pl_attr = player_attribute.player_mage()
+    elif pl_stat == 'healer':
+        pl_attr = player_attribute.player_healer()
+    else:
+        print('Please enter as specified!\n')
+        main()
+
+
+if __name__ == "__main__":
+    main()
