e4e34f0ab83222afbb788c4fa70c62c09da6100b
yanottamao <oktatama4@gmail.com>
2020-12-06 10:32:07 +0700

    Add initial enemies stat

---
+++ enemies_attribute.py
@@ -0,0 +1,27 @@
+# enemies stat
+
+'''
+Enemies stat guide
+
+Slime               health:     100
+                    attack      10 - 20
+                    stamina     10 - 20
+                    magic       10 - 20
+
+Goblin              health:     100
+                    attack      20 - 30
+                    stamina     10 - 20
+                    magic       10 - 20
+
+Hobgoblin           health:     100
+                    attack      10 - 20
+                    stamina     10 - 20
+                    magic       10 - 20
+
+Wolf                health:     100
+                    attack      10 - 20
+                    stamina     10 - 20
+                    magic       10 - 20
+
+To be added
+'''
